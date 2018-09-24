01f03dc811ace22ed4a9370899f37bd59c3ac595
mercedesb <mercedesbernard@live.com>
2018-09-24 09:07:23 -0500

    added express server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: ''
+  };
+   componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+   callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+     if (response.status !== 200) throw Error(body.message);
+     return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -11,7 +26,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.data}
         </p>
       </div>
     );
