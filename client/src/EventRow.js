ddaa9eac85e68f8896e94d90cfc69aa36058e269
mercedesb <mercedesbernard@live.com>
2018-09-25 22:34:08 -0500

    display continent and removed extra font family

--- client/src/EventRow.js
+++ client/src/EventRow.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export const EventRow = ({ name, url, location, city, country, countryCode, date, cfpClose, cfpUrl, eventTags }) => (
+export const EventRow = ({ name, url, location, city, country, countryCode, continent, date, cfpClose, cfpUrl, eventTags }) => (
   <div className="EventRow">
     <div className="EventRow-cell"><a href={url}>{name}</a></div>
-    <div className="EventRow-cell EventRow-cell--location">{location} <span className='u-small'>{city}, {country}, {countryCode}</span></div>
+    <div className="EventRow-cell EventRow-cell--location">
+      {location} 
+      <span className='u-small'>{city}, {country}, {countryCode}, {continent}</span>
+    </div>
     <div className="EventRow-cell">{date}</div>
     <div className="EventRow-cell"><a href={cfpUrl}>{cfpClose}</a></div>
     <div className="EventRow-cell">{eventTags}</div>
