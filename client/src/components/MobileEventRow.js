c71864f22b58f6577e541cbe39f3ea0ca9bddb01
mercedesb <mercedesbernard@live.com>
2018-09-27 16:53:07 -0500

    cleaned up mobile table styles

--- client/src/components/MobileEventRow.js
+++ client/src/components/MobileEventRow.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export const MobileEventRow = ({ className, cellClassName, event, isActive, onClick }) => (
-  <div className={`${className} MobileRow ${isActive ? 'MobileRow--active' : ''}`}>
-    <div className={`${cellClassName} MobileCell--header`} onClick={onClick}>
+  <div className={`${className} EventRow ${isActive ? 'EventRow--active' : ''}`}>
+    <div className={`${cellClassName} EventRow-cell EventRow-cell--header`} onClick={onClick}>
       <span><a href={event.url} target="_blank">{event.name}</a> - {event.date}</span>
       {isActive && <div className="Arrow Arrow--up"></div>}
       {!isActive && <div className="Arrow Arrow--down"></div>}
     </div>
-    <div className={`${cellClassName} MobileCell--body`}>
+    <div className={`${cellClassName} EventRow-cell EventRow-cell--body`}>
       {event.city}, {event.country} <br />
       <a href={event.cfpUrl} target="_blank">CFP Closes on {event.cfpClose}</a>
       <div className='u-small'>{event.eventTags}</div>
