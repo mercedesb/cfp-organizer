33cb08d6e038ba5e8af919a6657c03346f6e27bc
mercedesb <mercedesbernard@live.com>
2018-09-28 19:11:17 -0500

    make Map a functional component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.css';
 
-export class Map extends Component {
-  state = {
-    data: []
-  }
-
-  render() {
-    const position = [35, -20];
-    return (
-      <LeafletMap className="Map" center={position} zoom={3}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        { this.props.data && this.props.data.map((dataItem, i) => {
-          return (
-            <Marker position={[dataItem.lat, dataItem.lng]} key={i}>
-              <Popup>
-                {this.props.popupComponent(dataItem)}
-              </Popup>
-            </Marker>
-          )
-        })}
-      </LeafletMap>
-    );
-  }
-}
+const position = [35, -20];
+export const Map = ({ data, popupComponent }) => (
+  <LeafletMap className="Map" center={position} zoom={3}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    />
+    { data && data.map((dataItem, i) => {
+      return (
+        <Marker position={[dataItem.lat, dataItem.lng]} key={i}>
+          <Popup>
+            {popupComponent(dataItem)}
+          </Popup>
+        </Marker>
+      )
+    })}
+  </LeafletMap>
+);
