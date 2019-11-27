00eb90978108ca30c4ca3420a970e10d079ee623
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-11-27 10:29:43 -0600

    make test more flexible

--- server/confstech/test/parse.test.js
+++ server/confstech/test/parse.test.js
@@ -1,26 +1,29 @@
 //During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const fs = require('fs');
+const fs = require("fs");
 
-const parseEvents = require('../modules/parse');
+const parseEvents = require("../modules/parse");
 
 let eventFixture;
 function loadFixture() {
-  eventFixture = fs.readFileSync('server/confstech/test/eventFixture.txt', 'utf8')
+  eventFixture = fs.readFileSync(
+    "server/confstech/test/eventFixture.txt",
+    "utf8"
+  );
 }
 
-describe('parseEvents', () => {
+describe("parseEvents", () => {
   beforeEach(() => {
-    loadFixture()
-  })
+    loadFixture();
+  });
 
-  it('returns an array', () => {
+  it("returns an array", () => {
     expect(parseEvents(eventFixture)).toBeInstanceOf(Array);
   });
 
-  it('returns objects with the correct shape', () => {
-    const actual = parseEvents(eventFixture)[0]
+  it("returns objects with the correct shape", () => {
+    const actual = parseEvents(eventFixture)[0];
     expect(actual).toMatchObject({
       name: expect.any(String),
       location: expect.any(String),
@@ -29,25 +32,25 @@ describe('parseEvents', () => {
       cfpClose: expect.any(String),
       cfpUrl: expect.any(String),
       eventTags: expect.any(String)
-    })
-  })
+    });
+  });
 
-  it('returns objects with the correct data', () => {
-    const actual = parseEvents(eventFixture)[0]
+  it("returns objects with the correct data", () => {
+    const actual = parseEvents(eventFixture)[0];
     expect(actual).toMatchObject({
-      name: 'GDG DevFest',
-      location: 'Vienna, Austria',
-      date: 'November 24, 2018',
-      url: 'https://devfest.at',
-      cfpClose: 'October 26, 2019',
-      cfpUrl: 'https://devfest.at/call',
-      eventTags: 'android'
-    })
-  })
+      name: "GDG DevFest",
+      location: "Vienna, Austria",
+      date: "November 24, 2018",
+      url: "https://devfest.at",
+      cfpClose: expect.stringContaining("October 26, "),
+      cfpUrl: "https://devfest.at/call",
+      eventTags: "android"
+    });
+  });
 
-  it('returns dates in the correct format', () => {
-    const actual = parseEvents(eventFixture)[0]
-    expect(actual.date).toEqual(expect.stringMatching(/\w \d{1,2}, \d{4}/))
-    expect(actual.cfpClose).toEqual(expect.stringMatching(/\w \d{1,2}, \d{4}/))
-  })
-})
+  it("returns dates in the correct format", () => {
+    const actual = parseEvents(eventFixture)[0];
+    expect(actual.date).toEqual(expect.stringMatching(/\w \d{1,2}, \d{4}/));
+    expect(actual.cfpClose).toEqual(expect.stringMatching(/\w \d{1,2}, \d{4}/));
+  });
+});
